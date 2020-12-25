9cb9eb15c39b3cee1089f712d3353d63ada0aca4
Penata3 <ppenchev19@gmail.com>
2020-12-25 18:03:24 +0200

    Small fix

---
+++ Web/AutoHub.Web/Controllers/ReviewsController.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoHub.Web.Controllers
+{
+    using AutoHub.Services.Data;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ReviewsController : Controller
+    {
+        private readonly IReviewsService reviewsService;
+
+        public ReviewsController(IReviewsService reviewsService)
+        {
+            this.reviewsService = reviewsService;
+        }
+
+        [HttpGet]
+        public IActionResult All()
+        {
+            //var model = this.
+            return this.View();
+        }
+    }
+}
